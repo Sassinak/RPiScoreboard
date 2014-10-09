db2937a98d082f7c947547e7b728c6b4c5ce5c32
Sassinak <anabeldavidti@gmail.com>
2014-10-09 09:37:45 -0400

    clean up - removed test LEDs

--- script.js
+++ script.js
@@ -27,26 +27,18 @@
 			this.vis + ":" +
 			this.per
 			}
-		}
-	
-	var sequence; //test var
-	
+	}	
 
 // Controls and buttons
 // webiopi Calls
 
-function setupWebiopi(){
-	webiopi().setFunction(9,"out");	
-	sequence = "01010100110011001100101010";	
+function setupWebiopi(){	
 	count = CCOUNT;
 	display();
 }
 
-function callMacro(){	// dans script.py
-	webiopi().callMacro("SOSSequence",sequence);
-}
 function callMacroSerial(){
-	var args = etat.toString();  //["sequence?"];
+	var args = etat.toString(); 
 	var d = $("#output").text() + args + " ; ";
 	$("#output").text(d);
 	webiopi().callMacro("serialTX",args, printSerial);
@@ -54,13 +46,6 @@ function callMacroSerial(){
 function printSerial(macro, args, data){
 	var d = $("#output").text() + data + "\n";
 	$("#output").text(d);
-}
-function mousedown(){	//bouton "Hold"
-	webiopi().digitalWrite(9, 1);
-}
-
-function mouseup(){
-	webiopi().digitalWrite(9, 0);
 }
 
 //web UI calls
@@ -117,7 +102,6 @@ function timerPause(){
 	clearTimeout(timer);
 }
 
-
 function parseTime(time){
 	var second = time % 60;
 	var minute = Math.floor(time / 60) % 60;
@@ -130,22 +114,7 @@ function parseTime(time){
 function parseScore(score){
 	var value = ((score<10) ? '0' : '') + score;
 	return value;
-	}
-	
-/*function parseEtat(){ //alternative
-	//gather all values in one string;	
-	//"min:sec:home:vis:per"
-	var state =  parseTime(count);
-	state += ":";
-	state = state.replace (" ",":"); //compense le ":" flash
-	state += parseScore(homeScore);
-	state += ":";
-	state += parseScore(visScore);
-	state += ":";
-	state += periodeValue;
-	//send it off	
-	return state;
-}*/
+}	
 
 function display(){
 	//gather and parse vars
--- script.py
+++ script.py
@@ -6,65 +6,45 @@ import webiopi
 from webiopi.devices.serial import Serial
 
 GPIO = webiopi.GPIO
-LIGHT = 11 #GPIO pin using BCM numbering
-SEQ = 9
 
 serial = Serial("ttyAMA0", 9600)
 data = "niet"
 
-#sequence = '01010100110011001100101010'
-
 #setup called auto at webiopi startup
 def setup():
-	#set the GPIO used by the light to output
-	GPIO.setFunction(LIGHT, GPIO.OUT)
-	#GPIO.setFunction(SEQ, GPIO.OUT)
-	GPIO.digitalWrite(LIGHT, GPIO.HIGH)
-	#GPIO.digitalWrite(SEQ, GPIO.HIGH)
-
 	# empty input buffer before starting processing
-	while (serial.available() > 0):
+	if(serial.available() > 0):
 		serial.readString()
 
 def loop():
-	# Toggle LED each 5 seconds
-    value = not GPIO.digitalRead(LIGHT)
-    GPIO.digitalWrite(LIGHT, value)
+	#embetee: que mettre ici ? un led "en fonction" qui flashe ?
 
     webiopi.sleep(5)
 
 # destroy function is called at WebIOPi shutdown
-def destroy():
-    GPIO.digitalWrite(LIGHT, GPIO.LOW)
-    GPIO.digitalWrite(SEQ, GPIO.LOW)
+#def destroy():   ???
 
-# macros 
 
-@webiopi.macro
-def SOSSequence(sequence):
-	GPIO.outputSequence(SEQ, 100, sequence)
+# macro 
 	
 @webiopi.macro
 def serialTX(etat):           #solution en string
+	
+	# flush input buffer before starting processing
+	if(serial.available() > 0):
+		serial.readString()
+		
 	etat = re.sub('[:]', '', etat)
 	
 	txdata = "\n"		# LF ici (10 ou 0xA)	
 	txdata += etat
 	txdata += "\r"		# CR (13 ou 0xD)
-	serial.writeString(txdata)       # write a string
+	serial.writeString(txdata)       # write as string
 
 	data = "niet"
-	webiopi.sleep(1)				#sinon delai dans les donnees, bizarre
+	webiopi.sleep(0.25)				#sinon delai dans les donnees, bizarre
 	
 	if (serial.available() > 0):
 		data = serial.readString()        # read available data as string
-	#return returnData()
-	return data
 	
-
-@webiopi.macro
-def returnData():
-	data = ""
-	if (serial.available() > 0):
-		data = serial.readString()        # read available data as string
 	return data
